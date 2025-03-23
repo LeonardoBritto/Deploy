7c6454cb04d4457f347b0f2b3c00ddf7b3d2ac5c
Leonardo Brito <leonardobrito992@gmail.com>
2025-03-23 12:36:04 -0300

    Teste

--- Index.js
+++ Index.js
@@ -1,15 +1,22 @@
-import express from "express";
+const express = require('express');
+const path = require('path');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
+// Configura o Express para servir arquivos estáticos da pasta "images"
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Rota para retornar a imagem
+app.get('/cantada', (req, res) => {
+  res.sendFile(path.join(__dirname, 'images', 'cantada.jpg'));
+});
+
+// Rota padrão
 app.get('/', (req, res) => {
-res.json({message:"hello world"})
-})
-app.get('/api', (req, res) => {
-res.json({message:"you backend is deployed successfully! "});
-})
+  res.send('API Node.js funcionando!');
+});
 
-app.listen(3000, () =>{
-    console.log("Se acessou é GAY");
-})
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
